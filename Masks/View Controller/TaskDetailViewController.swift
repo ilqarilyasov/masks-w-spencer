fdc49f5b88da0ae88fc25eab12e3878daf9434e2
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-11 12:25:35 -0600

    Implement saveBarButtonTapped function

--- Masks/View Controller/TaskDetailViewController.swift
+++ Masks/View Controller/TaskDetailViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class TaskDetailViewController: UIViewController {
-
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var notesTextView: UITextView!
     
-    @IBAction func saveBarButtonTapped(_ sender: Any) {
-        
-    }
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func saveBarButtonTapped(_ sender: Any) {
+        
+        guard let name = nameTextField.text, !name.isEmpty else {
+            // Present an alert
+            return
+        }
+
+        let notes = notesTextView.text // Notes could be nil, it's optional. No need to unwrap
+        _ = Mask(name: name, notes: notes) // This create a task in ManagedObjectContext
+        
+        /// Where to put the task?
+        do {
+            let moc = CoreDataStack.shared.mainContext
+            try  moc.save() // This will take the tass from MOC and save it to PersistentStore
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+            navigationController?.popViewController(animated: true)
+        }
+    }
 }
