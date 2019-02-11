109d2a371e5836531f7f75cedaa455498db8eb89
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-02-11 11:22:15 -0600

    Lay out storyboard. Add outlets/actions and skeletons

---
+++ Masks/View Controller/TaskDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TaskDetailViewController.swift
+//  Masks
+//
+//  Created by Ilgar Ilyasov on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    @IBAction func saveBarButtonTapped(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ Masks/View Controller/TasksTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TasksTableViewController.swift
+//  Masks
+//
+//  Created by Ilgar Ilyasov on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class TasksTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+ 
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
